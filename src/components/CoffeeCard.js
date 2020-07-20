6f4f09d238b1bae1aca4becdad67b3408f899bda
aarnoli39 <aaronli39@gmail.com>
2020-07-20 17:43:18 -0400

    stuff

--- src/components/CoffeeCard.js
+++ src/components/CoffeeCard.js
@@ -1,55 +1,49 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { Avatar, IconButton, CardHeader, CardMedia } from "@material-ui/core";
+import ShareRoundedIcon from "@material-ui/icons/ShareRounded";
 
 const useStyles = makeStyles({
-    root: {
-        minWidth: 275,
-    },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-    },
-    title: {
-        fontSize: 14,
-    },
-    pos: {
-        marginBottom: 12,
-    },
+	imgHeight: {
+		height: "150px",
+	},
 });
 
-const CoffeeCard = () => {
-    const classes = useStyles();
-    const bull = <span className={classes.bullet}>•</span>;
+const CoffeeCard = (props) => {
+	const classes = useStyles();
+	const { avatarUrl, title, subtitle, description, imageUrl } = props;
 
-    return (
-        <Card className={classes.root}>
-            <CardContent>
-                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                    Word of the Day
-        </Typography>
-                <Typography variant="h5" component="h2">
-                    be{bull}nev{bull}o{bull}lent
-        </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                    adjective
-        </Typography>
-                <Typography variant="body2" component="p">
-                    well meaning and kindly.
-          <br />
-                    {'"a benevolent smile"'}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Learn More</Button>
-            </CardActions>
-        </Card>
-    );
-}
+	return (
+		<Card>
+			<CardHeader
+				avatar={<Avatar src={avatarUrl} />}
+				action={
+					<IconButton aria-label="settings">
+						<ShareRoundedIcon />
+					</IconButton>
+				}
+				title={title}
+				subheader={subtitle}
+			/>
 
-export default CoffeeCard
+			<CardMedia className={classes.imgHeight} image={imageUrl} />
+
+			<CardContent>
+				<Typography variant="body2" component="p">
+					{description}
+				</Typography>
+			</CardContent>
+			<CardActions>
+				<Button size="small">BUY NOW</Button>
+				<Button size="small">OFFER</Button>
+			</CardActions>
+		</Card>
+	);
+};
+
+export default CoffeeCard;
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import CoffeeCard from './CoffeeCard'
+import React from "react";
+import CoffeeCard from "./CoffeeCard";
+import { Grid } from "@material-ui/core";
+import coffeeMakerList from "./constants";
 
 const Content = () => {
-    return (
-        <CoffeeCard />
-    )
-}
+	const getCoffeeMakerCard = (coffeeMakerObj) => {
+		return (
+			<Grid item xs={12} sm={6} md={4}>
+				<CoffeeCard {...coffeeMakerObj} />
+			</Grid>
+		);
+	};
 
-export default Content
+	return (
+		<Grid spacing={4} container>
+			{coffeeMakerList.map((coffeeMakerObj) => (
+              getCoffeeMakerCard(coffeeMakerObj)
+            ))}
+		</Grid>
+	);
+};
+
+export default Content;
